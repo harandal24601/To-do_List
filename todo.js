4fdc628b9ebc863db9f177d8386d21d507e8a789
BrianLake <whddhr521@naver.com>
2021-06-09 21:06:45 +0900

    리스트출력기능

--- todo.js
+++ todo.js
@@ -3,10 +3,24 @@ function app() {
     // 삽입, 검색, 수정, 삭제
     return {
         schedule: new Array, // 스케줄 리스트
-        add = this.schedule.push,
-        // add: function (title) { // add(추가)
-        //     this.schedule.push(title);
-        // },
+        add: function (title) { // add(추가)
+            if (!this.schedule)
+                this.schedule = new Array;
+
+            this.schedule.push(title);
+        },
+        __str__: (obj) => {
+            let response = "";
+
+            console.log(obj);
+            
+            for (let i = 0; i < obj.schedule.length; i++) {
+                response += "<li>" + obj.schedule[i] + "</li>";
+                response += i < obj.schedule.length - 1 ? '\n' : '';
+            }
+
+            return response;
+        },
         delete: function (title) { // delete(삭제)
             let value = this.search(title);
             if (value >= 0)
@@ -29,10 +43,27 @@ function app() {
 
 let program = new app();
 
-let target = document.querySelector("#title").value;
+// program.add(target);
+// program.delete(target);
+// program.search(search);
+// program.modify(target, "ABC");
 
-program.add(target);
-program.delete(target);
-program.search(target);
-program.modify(target, "ABC");
+// 이벤트 추가
+document.querySelector("#search_button").onclick = () => { // 검색 버튼 클릭 이벤트
+    let search_input = document.getElementsByName("search_input")[0];
+    let search_value = search_input.value;
 
+    
+};
+
+document.querySelector("#insert_button").onclick = () => { // 추가 버튼 클릭 이벤트
+    let insert_input = document.getElementsByName("insert_input")[0];
+    let insert_value = insert_input.value;
+
+    program.add(insert_value);
+
+    console.log(program);
+    console.log(program.schedule);
+
+    document.querySelector("ul#schedule_table").innerHTML = program.__str__(program);
+};

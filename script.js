ccb49f091cdb749f399a1cd98cc1de4936c35de1
BrianLake <crwate521@gmail.com>
2021-06-11 17:47:15 +0900

    Todo 추가기능 구현

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//추가기능
+var button = document.getElementById('button');
+var input = document.getElementById('input');
+var list = document.getElementById('list');
+var cnt = 0; 
+
+button.addEventListener('click', clickButton);
+
+function clickButton() {
+    var ul_elem = document.createElement('ul');
+    var li_elem = document.createElement('li');
+
+    ul_elem.setAttribute('class', 'item');
+    ul_elem.setAttribute('id', 'list');
+    ul_elem.setAttribute('id', 'ul' + cnt);
+
+    li_elem.setAttribute('class', 'list-group-item');
+    li_elem.innerHTML = input.value;
+
+    ul_elem.appendChild(li_elem);
+
+    list.appendChild(ul_elem);
+
+    cnt++;
+}
+
 //검색기능
 
 function filter() {
